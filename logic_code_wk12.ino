8fb90e9c6c308e068abad510113cf3f8640f47e6
staralso <48972556+staralso@users.noreply.github.com>
2019-05-08 11:49:39 +0800

    Add files via upload

---
+++ logic_code_wk12.ino
@@ -0,0 +1,22 @@
+const int buttonPin=2;
+const int ledPin=13;
+int buttonstate=0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin,OUTPUT);
+pinMode(buttonPin,INPUT);
+digitalWrite(buttonPin,HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ buttonstate=digitalRead(buttonPin);
+ if( buttonstate==HIGH)
+ {
+  digitalWrite(ledPin,HIGH);
+  }
+ else
+ {
+  digitalWrite(ledPin,LOW);
+  }
+}
